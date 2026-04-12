cb6668d78fda3348625514847c2c5a5ee637806e
bharathidevopspro <bharathi.devops.pro@gmail.com>
2026-04-12 12:58:13 +0530

    Update variables.tf

--- terraform/main-project/variables.tf
+++ terraform/main-project/variables.tf
@@ -2,3 +2,7 @@ variable "region" {}
 variable "vpc_cidr" {}
 variable "instance_type" {}
 variable "ami_id" {}
+variable "public_subnets" {
+  description = "List of public subnet IDs"
+  type        = list(string)
+}
