564f5e746b35cb0b4ee1b3ca1e9aa89ee62cca49
bharathidevopspro <bharathi.devops.pro@gmail.com>
2026-04-12 20:01:50 +0530

    Update main.tf

--- terraform/main-project/main.tf
+++ terraform/main-project/main.tf
@@ -13,7 +13,6 @@ module "ec2" {
   source           = "./modules/ec2"  
   vpc_id           = module.vpc.vpc_id
   private_subnets  = module.vpc.private_subnets
-  public_subnets   = module.vpc.public_subnets
   instance_type    = var.instance_type
   ami_id           = var.ami_id
   target_group_arn = module.alb.target_group_arn
