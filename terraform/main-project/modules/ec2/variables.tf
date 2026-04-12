3b6ce0db056b1cac58c4d56c5967bb90eec8bf57
bharathidevopspro <bharathi.devops.pro@gmail.com>
2026-04-12 13:34:37 +0530

    Update variables.tf

--- terraform/main-project/modules/ec2/variables.tf
+++ terraform/main-project/modules/ec2/variables.tf
@@ -3,3 +3,7 @@ variable "private_subnets" {}
 variable "instance_type" {}
 variable "ami_id" {}
 variable "target_group_arn" {}
+variable "public_subnets" {
+  description = "List of public subnet IDs"
+  type        = list(string)
+}
