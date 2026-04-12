c42994b6a079093b4064594742712d81f05fbf64
bharathidevopspro <bharathi.devops.pro@gmail.com>
2026-04-12 19:28:09 +0530

    Update main.tf

--- terraform/main-project/modules/alb/main.tf
+++ terraform/main-project/modules/alb/main.tf
@@ -34,6 +34,9 @@ resource "aws_lb_listener" "listener" {
     target_group_arn = aws_lb_target_group.tg.arn
   }
 }
+output "alb_sg_id" {
+  value = aws_security_group.alb_sg.id
+}
 
 output "target_group_arn" {
   value = aws_lb_target_group.tg.arn
