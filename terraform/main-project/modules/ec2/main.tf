3860dc3dab4902bce1c21a5d97c365acecb85f7d
bharathidevopspro <bharathi.devops.pro@gmail.com>
2026-04-12 20:26:42 +0530

    Update main.tf

--- terraform/main-project/modules/ec2/main.tf
+++ terraform/main-project/modules/ec2/main.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "ec2_sg" {
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
-    security_groups = [var.alb_sg.id]   # later restrict to ALB
+    security_groups = [var.alb_sg_id]   # later restrict to ALB
   }
 }
 
